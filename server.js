81b53fc26b174d246e7fb7d6a40e51b1aa7e0e22
LycanLD <tranlongdo2506@gmail.com>
2025-07-29 18:41:10 +0700

    Improved theme, RT User counter, Join, Leave notification. Typing idicator.

--- server.js
+++ server.js
@@ -11,15 +11,35 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+const users = new Map();
+
 io.on('connection', (socket) => {
   console.log('a user connected:', socket.id);
+
+  socket.on('join', (username) => {
+    users.set(socket.id, username);
+    io.emit('user joined', { username, userCount: users.size });
+  });
 
   socket.on('chat message', (data) => {
     // data should be { username, message }
     io.emit('chat message', data);
   });
 
+  socket.on('typing', (username) => {
+    socket.broadcast.emit('typing', username);
+  });
+
+  socket.on('stop typing', (username) => {
+    socket.broadcast.emit('stop typing', username);
+  });
+
   socket.on('disconnect', () => {
+    const username = users.get(socket.id);
+    users.delete(socket.id);
+    if (username) {
+      io.emit('user left', { username, userCount: users.size });
+    }
     console.log('user disconnected:', socket.id);
   });
 });
